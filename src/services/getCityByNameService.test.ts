aea5b56f083ad2eb759cf06fbbcb38f1cac3e1bc
leokazuyukinagatani <lknlkn15@hotmail.com>
2023-08-22 20:26:46 -0300

    test: getCityByName e get WeatherByCity

---
+++ src/services/getCityByNameService.test.ts
@@ -0,0 +1,15 @@
+import { api } from "./api";
+import { getCityByNameService } from "./getCityByNameService";
+import { mockCityAPIResponse } from "@__tests__/mocks/mockCityAPIResponse";
+
+describe("Service: getCityByNameService", () => {
+  it("should return a city details", async () => {
+    jest.spyOn(api, "get").mockResolvedValue({
+      mockCityAPIResponse,
+    });
+
+    const response = await getCityByNameService("Toledo");
+
+    expect(response).toEqual(mockCityAPIResponse);
+  });
+});
