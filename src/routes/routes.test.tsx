a3c65dd668a29b0572096d82aeed7c05d4017675
leokazuyukinagatani <lknlkn15@hotmail.com>
2023-08-22 21:46:59 -0300

    feat: render

--- src/routes/routes.test.tsx
+++ src/routes/routes.test.tsx
@@ -1,5 +1,6 @@
 import { render, waitFor, screen } from "@testing-library/react-native";
 import { Routes } from "./";
+import { saveStorageCity } from "@libs/asyncStorage/cityStorage";
 
 describe("Routes", () => {
   it("should be render Search screen when not city selected", async () => {
@@ -8,4 +9,17 @@ describe("Routes", () => {
     const title = await waitFor(() => screen.findByText(/^escolha um local/i));
     expect(title).toBeTruthy();
   });
+
+  it('should be render Dashboard screen when city selected',async()=>{
+    const city = {
+      id: '1',
+      name: 'São Paulo',
+      latitude: 123,
+      longitude: 456
+    }
+
+    await saveStorageCity(city)
+    render(<Routes />)
+
+  })
 });
