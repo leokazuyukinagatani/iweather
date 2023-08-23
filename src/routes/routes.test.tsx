978b411d65e77c701a03ee6c8028e699986abdda
leokazuyukinagatani <lknlkn15@hotmail.com>
2023-08-22 21:40:41 -0300

    test: routes with not city selected

---
+++ src/routes/routes.test.tsx
@@ -0,0 +1,11 @@
+import { render, waitFor, screen } from "@testing-library/react-native";
+import { Routes } from "./";
+
+describe("Routes", () => {
+  it("should be render Search screen when not city selected", async () => {
+    render(<Routes />);
+
+    const title = await waitFor(() => screen.findByText(/^escolha um local/i));
+    expect(title).toBeTruthy();
+  });
+});
