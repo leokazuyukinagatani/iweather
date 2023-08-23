86b2473e7bc76f56513a77e77d90f27265ef6391
leokazuyukinagatani <lknlkn15@hotmail.com>
2023-08-22 21:31:01 -0300

    test: context

---
+++ src/contexts/cityContext.test.tsx
@@ -0,0 +1,24 @@
+import { act, renderHook, waitFor } from "@testing-library/react-native";
+
+import { useCity } from "@hooks/useCity";
+import { CityProvider } from "./CityContext";
+describe("Context: CityContext", () => {
+  it("should be change selected city", async () => {
+    const { result } = renderHook(() => useCity(), {
+      wrapper: CityProvider,
+    });
+
+    await waitFor(() =>
+      act(() =>
+        result.current.handleChanceCity({
+          id: "1",
+          name: "São Paulo",
+          latitude: 123,
+          longitude: 123,
+        })
+      )
+    );
+
+    expect(result.current.city?.name).toBe("São Paulo");
+  });
+});
