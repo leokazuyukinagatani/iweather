d647ae27269a3c9d58b18eb4903f0f9cd63117e7
leokazuyukinagatani <lknlkn15@hotmail.com>
2023-08-22 21:21:48 -0300

    text: weatherItem

---
+++ src/components/WeatherItem/weatherItem.test.tsx
@@ -0,0 +1,17 @@
+import { WeatherItem } from "@components/WeatherItem";
+import { render,screen } from "@testing-library/react-native";
+import dropIcon from "@assets/drop.svg";
+describe("Component: WeatherItem", () => {
+  it("should be show title and value", () => {
+    render(<WeatherItem icon={dropIcon} title="Umidade do Ar" value="81%" />);
+  });
+
+  const title = screen.getByText("Umidade do Ar");
+  const value = screen.getByText("81%");
+  
+  expect(title).toBeTruthy();
+  expect(value).toBeTruthy();
+
+
+  
+});
